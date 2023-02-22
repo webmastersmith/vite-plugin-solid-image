42cef0a09a21921f74c5cebb83fe0c9748544d76
Bryon Smith <webmastersmith37@gmail.com>
2023-02-22 16:15:57 -0600

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,82 @@
 # vite-plugin-solid-image
+
+## What it does?
+
+This plugin automates my `solid-image` package for vite. I built this for plugin for solid-start framework.
+
+## Example
+
+**vite.config.ts**
+
+```ts
+import { defineConfig } from 'vite';
+import { solidImage } from 'vite-plugin-solid-image';
+
+export default defineConfig({
+  plugins: [solidImage()],
+});
+```
+
+**YourFile.tsx**
+
+```tsx
+import { createImages } from 'solid-image';
+
+export default function Pic() {
+  return (
+    <>
+      <h1 class={styles.pic}>Pic Component</h1>
+      {createImages([
+        'src/components/Pic/phone.png?w=20;120&f=avif;webp&progressBar=false&print=false&media=(min-width: 1px)',
+        'src/components/Pic/phone.png?w=25;125&f=avif;webp&progressBar=false&print=false&media=(min-width: 1px)',
+        'src/components/Pic/phone.png?w=55;155&f=avif;webp&progressBar=false&print=false&media=(min-width: 1px)',
+      ])}
+    </>
+  );
+}
+```
+
+### Folder Structure
+
+```txt
+- root
+  - src
+    - components
+    - routes
+    - ...
+```
+
+- this plugin only look for `.jsx, tsx` files in the `src` folder.
+
+### Comments
+
+- `{/* { createImages(...) } */}` // will not be processed.
+
+### URL restrictions
+
+- the url can only be in string format.
+  - `'src/components/Pic/phone.png?w=20;120&f=avif;webp`
+- image paths must be relative to root.
+- **src folder**
+  - `src/yourFolderPath/imageName.jpg`
+- **public folder**
+  - `public/yourFolderPath/imageName.jpg`
+
+```tsx
+<Image src={'src/components/Pic/phone.png?w=50;100;150&f=avif&progressBar=false'} />
+// or
+<Image
+  src={[
+    'src/components/Pic/phone.png?w=50;100;150&f=avif;webp&progressBar=false',
+    'src/components/Pic/phone.png?w=50;100;150&f=avif;webp&progressBar=false',
+    'src/components/Pic/phone.png?w=50;100;150&f=avif;webp&progressBar=false',
+  ]}
+/>
+```
+
+# URL Options
+
+- see: <https://github.com/webmastersmith/solid-image#url-options>
+- progressBar and output to console.log are turned off by default.
+  - to turn on:
+    - `progressBar=true&print=true`
---
+++ src/index.ts
@@ -0,0 +1,85 @@
+import { createImages } from 'solid-image';
+import path from 'node:path';
+import fs from 'fs';
+export default createImages;
+
+export function solidImage() {
+  return {
+    name: 'vite-plugin-solid-image', // this name will show up in warnings and errors
+    async load(id: string) {
+      const basePath = path.join(process.cwd(), 'src');
+      if (id.includes(basePath)) {
+        return parseFile(id);
+      }
+      return;
+    },
+  };
+}
+
+async function parseFile(id: string) {
+  // only js, jsx, ts, tsx files
+  if (/[jt]sx$/i.test(id)) {
+    // file is (tj)sx read it for 'Image'
+    let file = fs.readFileSync(id, 'utf-8');
+
+    // look for this pattern.
+    let solidImageRegex = new RegExp('import .*?createImages.*', 'ig');
+    // check if 'import solid-image' is in file.
+    if (solidImageRegex.test(file)) {
+      // console.log(file);
+      // lookbehind to ignore commented out code.
+      const createImagesRegex = new RegExp(/(?<!\{\/\*.?){createImages(.*?)}/is);
+      // console.log(createImagesRegex);
+      // prevent runaway loop
+      let count = 300;
+      while (createImagesRegex.test(file) && count > 0) {
+        const imageMatch = file.match(createImagesRegex)!;
+        // console.log('imageMatch', imageMatch);
+        const index = imageMatch.index!;
+        const endIndex = index + imageMatch[0].length + 1;
+        // console.log(index, endIndex);
+        const front = file.slice(0, index);
+        // console.log('front', front);
+        const back = file.slice(endIndex, file.length);
+        // console.log('back', back);
+        // normalize urls, remove blank strings.
+        const url = imageMatch[1]
+          .replaceAll(/[^-_a-zA-Z0-9:;,=\/&?.]/gi, '')
+          .split(',')
+          .reduce((a: string[], b: string) => {
+            // remove empty strings.
+            if (b) {
+              a.push(b);
+            }
+            return a;
+          }, []);
+        // console.log(url);
+
+        // test for turning on of print or progressBar
+        const urlJoin = url.join('');
+        const progressBar = `&progressBar=${/progressBar=true/.test(urlJoin)}`;
+        const print = `&print=${/print=true/.test(urlJoin)}`;
+        // loop each url and replace.
+        const urlFix = url.map((u: string) => {
+          // avoid making duplicate entries in the url.
+          const _url = u
+            .replaceAll(/&progressBar=(true|false)/gi, '')
+            .replaceAll(/&print=(true|false)/gi, '');
+          return _url + progressBar + print;
+        });
+        // console.log('urlFix', urlFix);
+
+        const img = await createImages(urlFix);
+        // console.log('return createImages', img);
+        file = front + `\n${img}\n` + back;
+        // reduce count
+        count--;
+      }
+      file = file.replaceAll(solidImageRegex, '');
+      // console.log(file);
+      return file;
+    }
+    return;
+  }
+  return;
+}
